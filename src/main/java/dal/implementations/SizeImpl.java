97a8f0c16634921f84456e92aeee0f4a6c7eb0e7
AlexKolc <alex_kolc@inbox.ru>
2019-04-03 01:15:48 +0300

    Added SizeDao and SizeImpl

---
+++ src/main/java/dal/implementations/SizeImpl.java
@@ -0,0 +1,90 @@
+package dal.implementations;
+
+import dal.interfaces.SizeDao;
+import entity.Size;
+import utils.ConnectionUtil;
+
+import java.sql.*;
+import java.util.ArrayList;
+import java.util.List;
+
+public class SizeImpl implements SizeDao {
+    @Override
+    public void add(Size size) {
+        String query = "INSERT INTO SIZE(SIZENAME) VALUES(?)";
+        try (Connection connection = ConnectionUtil.getConnection()) {
+            PreparedStatement preparedStatement = connection.prepareStatement(query);
+            preparedStatement.setString(1, size.getSizeName());
+            preparedStatement.executeUpdate();
+        } catch (SQLException ex) {
+            ex.printStackTrace();
+        }
+    }
+
+    @Override
+    public List<Size> getAll() {
+        String query = "SELECT * FROM SIZE";
+        List<Size> sizes = new ArrayList<>();
+        try (Connection connection = ConnectionUtil.getConnection()) {
+            Statement statement = connection.createStatement();
+            ResultSet rs = statement.executeQuery(query);
+            while (rs.next()) {
+                Size size = new Size();
+                try {
+                    size.setSizeId(rs.getInt("SIZEID"));
+                    size.setSizeName(rs.getString("SIZENAME"));
+                } catch (SQLException ex) {
+                    ex.printStackTrace();
+                }
+                sizes.add(size);
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+        return sizes;
+    }
+
+    @Override
+    public Size getById(Integer id) {
+        String query = "SELECT * FROM SIZE WHERE SIZEID = ?";
+        Size size = null;
+        try (Connection connection = ConnectionUtil.getConnection()) {
+            PreparedStatement preparedStatement = connection.prepareStatement(query);
+            preparedStatement.setInt(1, id);
+            ResultSet rs = preparedStatement.executeQuery();
+            if (rs.next()) {
+                size = new Size();
+                size.setSizeId(rs.getInt("SIZEID"));
+                size.setSizeName(rs.getString("SIZENAME"));
+            }
+        } catch (SQLException ex) {
+            ex.printStackTrace();
+        }
+        return size;
+    }
+
+    @Override
+    public void update(Size size) {
+        String query = "UPDATE SIZE SET SIZENAME = ? WHERE SIZEID = ?";
+        try (Connection connection = ConnectionUtil.getConnection()) {
+            PreparedStatement preparedStatement = connection.prepareStatement(query);
+            preparedStatement.setString(1, size.getSizeName());
+            preparedStatement.setInt(2, size.getSizeId());
+            preparedStatement.executeUpdate();
+        } catch (SQLException ex) {
+            ex.printStackTrace();
+        }
+    }
+
+    @Override
+    public void delete(Size size) {
+        String query = "DELETE FROM SIZE WHERE SIZEID = ?";
+        try (Connection connection = ConnectionUtil.getConnection()) {
+            PreparedStatement preparedStatement = connection.prepareStatement(query);
+            preparedStatement.setInt(1, size.getSizeId());
+            preparedStatement.executeUpdate();
+        } catch (SQLException ex) {
+            ex.printStackTrace();
+        }
+    }
+}
