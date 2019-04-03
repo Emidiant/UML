d46d2abb40309927d2e28d52bfb49fe4b6fd0930
Emidiant <yulia-sergeeva1999@yandex.ru>
2019-04-03 14:36:01 +0300

    hellopage должен открыться

--- src/main/java/ru/ifmo/uml/ui/MainController.java
+++ src/main/java/ru/ifmo/uml/ui/MainController.java
@@ -1,92 +1,58 @@
 package ru.ifmo.uml.ui;
 
-import ru.ifmo.uml.dal.implementations.OrderImpl;
-import ru.ifmo.uml.dal.implementations.ProductImpl;
-import ru.ifmo.uml.dal.implementations.ProductOrderImpl;
-import ru.ifmo.uml.dal.implementations.SizeImpl;
-import ru.ifmo.uml.entity.Order;
-import ru.ifmo.uml.entity.Product;
-import ru.ifmo.uml.entity.ProductOrder;
-import ru.ifmo.uml.entity.Size;
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.image.Image;
+import javafx.scene.layout.BorderPane;
+import javafx.stage.Stage;
+
+import java.io.IOException;
 
 
-public class MainController {
+public class MainController extends Application {
+    private Stage primaryStage;
+    private BorderPane mainLayout;
+    private Stage prevStage;
+
+    @Override
+    public void start(Stage stage) throws IOException {
+
+        this.primaryStage = stage;
+        //this.primaryStage.getIcons().add(new Image("/image/gamecontroller.png"));
+        this.primaryStage.setTitle(" Start page");
+        showMainPage();
+
+        FXMLLoader loader = new FXMLLoader();
+
+        Parent root = (Parent) loader.load(MainController.class.getResourceAsStream("/fxml/hellopage.fxml"));
+        MainController mainController = loader.getController();
+        //mainController.setStage(primaryStage);
+
+        Scene scene = new Scene(root);
+        primaryStage.setScene(scene);
+        primaryStage.show();
+
+
+    }
+
+    public void showMainPage() {
+        FXMLLoader loader = new FXMLLoader();
+        try {
+            Parent root = loader.load(MainController.class.getResourceAsStream("/fxml/hellopage.fxml"));
+            primaryStage.setScene(new Scene(root));
+            primaryStage.show();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+
+
+
     public static void main(String[] args) {
-        //тест
-        //size+
-        //booking+
-        //product+
-        //product order
-
-        ProductOrderImpl productOrderImpl = new ProductOrderImpl();
-        ProductOrder productOrder = new ProductOrder();
-
-        //productOrder.setOrderId(1);
-        //productOrder.setCount(2);
-        //productOrder.setProductId(2);
-        //Pair<Integer, Integer> pair = new Pair<>(2, 1);
-        //System.out.println(productOrderImpl.getById(pair));
-
-        //productOrderImpl.update(productOrder);
-
-        SizeImpl sizeImpl = new SizeImpl();
-        Size size = new Size();
-        size.setSizeName("xxl");
-
-        ProductImpl productImpl = new ProductImpl();
-        Product product = new Product();
-
-        Order order = new Order();
-        OrderImpl orderImpl = new OrderImpl();
-
-
-        //get by ID
-        Size seze1 = sizeImpl.getById(2);
-        System.out.println(seze1.toString());
-        System.out.println("\n");
-        Product product1 = productImpl.getById(2);
-        System.out.println(product1.toString());
-        System.out.println(orderImpl.getById(1));
-
-        //ADD
-        //sizeImpl.add(size);
-        //bookingImpl.add(booking);
-        //productImpl.add(product);
-
-        //UPDATE
-        //size.setSizeName("XXL");
-        //size.setSizeId(33);
-        //sizeImpl.update(size);
-        //bookingImpl.update(booking);
-        //product.setProductId(34);
-        //productImpl.update(product);
-
-        //DELETE
-        //sizeImpl.delete(size);
-        //bookingImpl.delete(booking);
-        //productImpl.delete(product);
-
-        //GET ALL
-        /*List<Color> colorList = colorImpl.getAll();
-           for (Color c : colorList){
-               System.out.println(c);
-           }*/
-        /*List<Size> sizeList = sizeImpl.getAll();
-        for (Size s : sizeList){
-            System.out.println(s);
-        }*/
-        /*List<Booking> bookingList = bookingImpl.getAll();
-        for (Booking o : bookingList){
-            System.out.println(o);
-        }*/
-        /*List<Product> productList = productImpl.getAll();
-        for (Product p : productList){
-            System.out.println(p);
-        }*/
-        /*List<ProductOrder> productOrderList = productOrderImpl.getAll();
-        for (ProductOrder p : productOrderList){
-            System.out.println(p);
-        }*/
+        launch(args);
     }
 
 
