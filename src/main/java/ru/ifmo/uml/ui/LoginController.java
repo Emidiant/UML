435884a491e8045407f535f7d24f82f091dd464a
Emidiant <yulia-sergeeva1999@yandex.ru>
2019-04-03 14:54:56 +0300

    mainApp вместо main Controller

--- src/main/java/ru/ifmo/uml/ui/LoginController.java
+++ src/main/java/ru/ifmo/uml/ui/LoginController.java
@@ -1,4 +1,5 @@
 package ru.ifmo.uml.ui;
 
 public class LoginController {
+
 }
--- src/main/java/ru/ifmo/uml/ui/MainController.java
+++ src/main/java/ru/ifmo/uml/ui/MainController.java
@@ -1,60 +1,14 @@
 package ru.ifmo.uml.ui;
 
-import javafx.application.Application;
-import javafx.fxml.FXMLLoader;
-import javafx.scene.Parent;
-import javafx.scene.Scene;
-import javafx.scene.image.Image;
-import javafx.scene.layout.BorderPane;
+
 import javafx.stage.Stage;
 
-import java.io.IOException;
+public class MainController {
 
-
-public class MainController extends Application {
-    private Stage primaryStage;
-    private BorderPane mainLayout;
-    private Stage prevStage;
-
-    @Override
-    public void start(Stage stage) throws IOException {
-
-        this.primaryStage = stage;
-        //this.primaryStage.getIcons().add(new Image("/image/gamecontroller.png"));
-        this.primaryStage.setTitle(" Start page");
-        showMainPage();
-
-        FXMLLoader loader = new FXMLLoader();
-
-        Parent root = (Parent) loader.load(MainController.class.getResourceAsStream("/fxml/hellopage.fxml"));
-        MainController mainController = loader.getController();
-        //mainController.setStage(primaryStage);
-
-        Scene scene = new Scene(root);
-        primaryStage.setScene(scene);
-        primaryStage.show();
-
-
+    Stage prevStage;
+    public void setStage(Stage stage){
+        this.prevStage = stage;
     }
-
-    public void showMainPage() {
-        FXMLLoader loader = new FXMLLoader();
-        try {
-            Parent root = loader.load(MainController.class.getResourceAsStream("/fxml/hellopage.fxml"));
-            primaryStage.setScene(new Scene(root));
-            primaryStage.show();
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-    }
-
-
-
-
-    public static void main(String[] args) {
-        launch(args);
-    }
-
 
 }
 
