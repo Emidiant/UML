cdfe9455134f93277f95dac783991c7975f612d7
Vladimir Romaniuk <romvr99@yandex.ru>
2019-04-08 16:18:07 +0300

    Changed login page

--- src/main/java/ru/ifmo/uml/ui/LoginController.java
+++ src/main/java/ru/ifmo/uml/ui/LoginController.java
@@ -1,10 +1,25 @@
 package ru.ifmo.uml.ui;
 
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.PasswordField;
+import javafx.scene.control.TextField;
+import javafx.scene.image.ImageView;
 import javafx.stage.Stage;
 
 public class LoginController {
+    //TODO back,login btn
+    public TextField txtUsername;
+    public PasswordField passPassword;
+    public Button btnLogin;
+    public Button btnBack;
+    public ImageView imgBig;
     Stage prevStage;
     public void setStage(Stage stage){
         this.prevStage = stage;
+    }
+    @FXML
+    void initialize(){
+
     }
 }
