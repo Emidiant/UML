959d242ee61d5ac5185f9d1f2aecfdf87a449d70
AlexKolc <alex_kolc@inbox.ru>
2019-04-14 19:13:09 +0300

    Added ComboBox to select admin mode

--- src/main/java/ru/ifmo/uml/ui/LoginController.java
+++ src/main/java/ru/ifmo/uml/ui/LoginController.java
@@ -2,9 +2,7 @@ package ru.ifmo.uml.ui;
 
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
-import javafx.scene.control.Button;
-import javafx.scene.control.PasswordField;
-import javafx.scene.control.TextField;
+import javafx.scene.control.*;
 import javafx.scene.image.ImageView;
 import javafx.stage.Stage;
 import ru.ifmo.uml.dal.implementations.AdministratorImpl;
@@ -14,8 +12,10 @@ public class LoginController {
     //TODO back,login btn
     public TextField txtUsername;
     public PasswordField passPassword;
+    public ComboBox<String> comboBox;
     public Button btnLogin;
     public Button btnBack;
+    public TextField incorrectData;
     public ImageView imgBig;
     public Administrator administrator = null;
     private AdministratorImpl auth;
@@ -27,9 +27,34 @@ public class LoginController {
     void initialize(){
         auth = new AdministratorImpl();
 
-        //временно
+        if (comboBox.getItems().isEmpty()) {
+            comboBox.getItems().addAll("Admin", "Coordinator", "Logistician");
+        }
+
+        txtUsername.textProperty().addListener(
+                (observable, oldValue, newValue) -> {
+                    Administrator tempAdministrator = auth.getByLogin(txtUsername.getText());
+                    if (tempAdministrator == null) {
+                        comboBox.setValue(null);
+                        comboBox.setDisable(true);
+                    } else if (tempAdministrator.getLevel().equals("logist")) {
+                        comboBox.setValue("Logistician");
+                    } else if (tempAdministrator.getLevel().equals("low")) {
+                        comboBox.setValue("Coordinator");
+                    } else if (tempAdministrator.getLevel().equals("admin")) {
+                        comboBox.setDisable(false);
+                    }
+                });
+
+        //временнo
+        //COORDINATOR
         txtUsername.setText("morgan");
+        //LOGICIAN
+        //txtUsername.setText("iivanov");
+        //GOD
+        //txtUsername.setText("admin");
         passPassword.setText("123654");
+
     }
 
     @FXML
@@ -39,12 +64,32 @@ public class LoginController {
 
     public void btnLoginClicked(ActionEvent actionEvent) {
         Administrator tempAdministrator = auth.getByLogin(txtUsername.getText());
-        if (tempAdministrator.getPassword().equals(passPassword.getText())) {
-            administrator = tempAdministrator;
-            //в зависимости от уровня открывать coordinator page или logistic page
-            MainApp.showCoordPage();
+        try {
+            if (tempAdministrator.getPassword().equals(passPassword.getText())) {
+                administrator = tempAdministrator;
+                //в зависимости от уровня открывать coordinator page или logistic page
+                if (administrator.getLevel().equals("admin")) {
+                    try {
+                        if (comboBox.getValue().equals("Admin")) {
+                            //TODO: showAdminPage
+                        } else if (comboBox.getValue().equals("Coordinator")) {
+                            MainApp.showCoordPage();
+                        } else if (comboBox.getValue().equals("Logistician")) {
+                            MainApp.showLogistPage();
+                        }
+                    } catch (NullPointerException e) {
+                        incorrectData.setText("Choose mode");
+                    }
 
-            System.out.println("Login successfully");
+                } else if (administrator.getLevel().equals("low")) {
+                    MainApp.showCoordPage();
+                } else if (administrator.getLevel().equals("logist")) {
+                    MainApp.showLogistPage();
+                }
+
+            }
+        } catch (NullPointerException e) {
+            incorrectData.setText("Incorect name or password");
         }
     }
 }
